f4df41c6eb135fdcdeab7d9dc036f0d1017cc309
LfullStack <quinterolevii87@gmail.com>
2026-09-24 19:45:54 -0500

    Configura base de datos por variables de entorno y limpia archivos de cache

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'portsense-secret-key-2024'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'mysql+pymysql://root:Cklif*2005@localhost/portsense'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'portsense-secret-key-2026'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///portsense_local.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
